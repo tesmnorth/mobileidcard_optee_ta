5ed83ac976f3fd775b975374bed922d9284f7c12
Tesmnorth <tesmnorth@gmail.com>
2018-05-07 16:46:50 +0300

    test

--- ta/mobileidcard_ta.c
+++ ta/mobileidcard_ta.c
@@ -253,7 +253,7 @@ static TEE_Result get_public_key(uint32_t param_types, TEE_Param params[4])
 	memcpy(modulus, bigIntMod, (bigIntLen * sizeof(TEE_BigInt)));
 
 	for (long unsigned int i = 0; i < (bigIntLen * sizeof(TEE_BigInt)); i++) {
-		DMSG("i : %d value : %d", i, exponent[i]);
+		DMSG("i : %lu value : %d", i, exponent[i]);
 	}
 
 	cleanup1:
