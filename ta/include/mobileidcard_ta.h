6f15de43cb971a3593ac7d29325f9ea0b20b1056
Tesmnorth <tesmnorth@gmail.com>
2018-10-22 18:11:11 +0300

    added functions about signed pk

--- ta/include/mobileidcard_ta.h
+++ ta/include/mobileidcard_ta.h
@@ -4,11 +4,16 @@
 #define TA_MIC_UUID { 0xc4d22850, 0x20f1, 0x4b12, \
 					{0x82, 0x3a, 0xeb, 0x7a, 0xef, 0xba, 0xe4, 0x62} }
 
-#define TA_GENERATE_KEY_CMD 0
-#define TA_DELETE_KEY_CMD 1
+#define TA_GENERATE_RSA_KEY_CMD 0
+#define TA_DELETE_RSA_KEY_CMD 1
 #define TA_GET_PUBLICKEY_EXP_MOD_CMD 2
 #define TA_VERIFY_CMD 3
+#define TA_DELETE_SIGNED_PUBLIC_KEY_CMD 4
+#define TA_SAVE_SIGNED_PUBLIC_KEY_CMD 5
+#define TA_GET_SIGNED_PUBLIC_KEY_CMD 6
 
-#define KEY_SIZE 2048
+#define RSA_KEY_SIZE 2048
+#define RSA_KEY_ID 100
+#define SIGNED_PUBLIC_KEY_ID 101
 
 #endif
--- ta/mobileidcard_ta.c
+++ ta/mobileidcard_ta.c
@@ -57,7 +57,7 @@ void TA_CloseSessionEntryPoint(void * sess_ctx)
 	DMSG("Goodbye!\n");
 }
 
-static TEE_Result generate_and_save_key(uint32_t param_types, TEE_Param params[4])
+static TEE_Result generate_and_save_rsa_key(uint32_t param_types, TEE_Param params[4])
 {
 	TEE_Result result = TEE_SUCCESS;
 	TEE_ObjectHandle transientKey = (TEE_ObjectHandle)NULL;
@@ -73,29 +73,29 @@ static TEE_Result generate_and_save_key(uint32_t param_types, TEE_Param params[4
 			TEE_DATA_FLAG_SHARE_WRITE;
 
 	uint32_t exp_param_types = TEE_PARAM_TYPES(
-			TEE_PARAM_TYPE_VALUE_INPUT,
 			TEE_PARAM_TYPE_VALUE_OUTPUT,
+			TEE_PARAM_TYPE_NONE,
 			TEE_PARAM_TYPE_NONE,
 			TEE_PARAM_TYPE_NONE);
 
-	keyId = params[0].value.a;
+	keyId = RSA_KEY_ID;
 
 	if (param_types != exp_param_types)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	result = TEE_AllocateTransientObject(TEE_TYPE_RSA_KEYPAIR, KEY_SIZE, &transientKey);
+	result = TEE_AllocateTransientObject(TEE_TYPE_RSA_KEYPAIR, RSA_KEY_SIZE, &transientKey);
 
 	if (result != TEE_SUCCESS) {
 		EMSG("Failed to allocate transient object handle : 0x%x", result);
-		params[1].value.a = 0;
+		params[0].value.a = 0;
 		goto cleanup;
 	}
 
-	result = TEE_GenerateKey(transientKey, KEY_SIZE, NULL, 0);
+	result = TEE_GenerateKey(transientKey, RSA_KEY_SIZE, NULL, 0);
 
 	if (result != TEE_SUCCESS) {
 		EMSG("Failed to generate a transient key: 0x%x", result);
-		params[1].value.a = 0;
+		params[0].value.a = 0;
 		goto cleanup1;
 	}
 
@@ -105,7 +105,7 @@ static TEE_Result generate_and_save_key(uint32_t param_types, TEE_Param params[4
 
 	if (result != TEE_SUCCESS) {
 		EMSG("Failed to create a persistent key: 0x%x", result);
-		params[1].value.a = 0;
+		params[0].value.a = 0;
 		goto cleanup1;
 	}
 
@@ -114,11 +114,11 @@ static TEE_Result generate_and_save_key(uint32_t param_types, TEE_Param params[4
 
 	if (result != TEE_SUCCESS) {
 		EMSG("keyInfo and keyInfo2 are different");
-		params[1].value.a = 0;
+		params[0].value.a = 0;
 		goto cleanup2;
 	}
 
-	params[1].value.a = 1;
+	params[0].value.a = 1;
 
 	cleanup2:
 	TEE_CloseObject(persistentKey);
@@ -128,7 +128,7 @@ static TEE_Result generate_and_save_key(uint32_t param_types, TEE_Param params[4
 	return result;
 }
 
-static TEE_Result delete_key(uint32_t param_types, TEE_Param params[4])
+static TEE_Result delete_rsa_key(uint32_t param_types, TEE_Param params[4])
 {
 	TEE_Result result = TEE_SUCCESS;
 	TEE_ObjectHandle key_handle = (TEE_ObjectHandle)NULL;
@@ -141,12 +141,12 @@ static TEE_Result delete_key(uint32_t param_types, TEE_Param params[4])
 			TEE_DATA_FLAG_SHARE_WRITE;
 
 	uint32_t exp_param_types = TEE_PARAM_TYPES(
-			TEE_PARAM_TYPE_VALUE_INPUT,
 			TEE_PARAM_TYPE_VALUE_OUTPUT,
+			TEE_PARAM_TYPE_NONE,
 			TEE_PARAM_TYPE_NONE,
 			TEE_PARAM_TYPE_NONE);
 
-	keyId = params[0].value.a;
+	keyId = RSA_KEY_ID;
 
 	if (param_types != exp_param_types)
 		return TEE_ERROR_BAD_PARAMETERS;
@@ -157,12 +157,52 @@ static TEE_Result delete_key(uint32_t param_types, TEE_Param params[4])
 	if (result != TEE_SUCCESS)
 	{
 		EMSG("Failed to open object handle : 0x%x", result);
-		params[1].value.a = 0;
+		params[0].value.a = 0;
 		goto cleanup;
 	}
 
-	params[1].value.a = 1;
+	params[0].value.a = 1;
 	TEE_CloseAndDeletePersistentObject(key_handle);
+
+	cleanup:
+	return result;
+}
+
+static TEE_Result delete_signed_public_key(uint32_t param_types, TEE_Param params[4])
+{
+	TEE_Result result = TEE_SUCCESS;
+	TEE_ObjectHandle signed_public_key_handle = (TEE_ObjectHandle)NULL;
+	uint32_t signedPublicKeyId = 0;
+
+	uint32_t flags = TEE_DATA_FLAG_ACCESS_READ |
+			TEE_DATA_FLAG_ACCESS_WRITE |
+			TEE_DATA_FLAG_ACCESS_WRITE_META |
+			TEE_DATA_FLAG_SHARE_READ |
+			TEE_DATA_FLAG_SHARE_WRITE;
+
+	uint32_t exp_param_types = TEE_PARAM_TYPES(
+			TEE_PARAM_TYPE_VALUE_OUTPUT,
+			TEE_PARAM_TYPE_NONE,
+			TEE_PARAM_TYPE_NONE,
+			TEE_PARAM_TYPE_NONE);
+
+	signedPublicKeyId = SIGNED_PUBLIC_KEY_ID;
+
+	if (param_types != exp_param_types)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	result = TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE, &signedPublicKeyId, sizeof(signedPublicKeyId),
+			flags, &signed_public_key_handle);
+
+	if (result != TEE_SUCCESS)
+	{
+		EMSG("Failed to open object handle : 0x%x", result);
+		params[0].value.a = 0;
+		goto cleanup;
+	}
+
+	params[0].value.a = 1;
+	TEE_CloseAndDeletePersistentObject(signed_public_key_handle);
 
 	cleanup:
 	return result;
@@ -188,20 +228,20 @@ static TEE_Result get_public_key_exponent_modulus(uint32_t param_types, TEE_Para
 			TEE_DATA_FLAG_SHARE_WRITE;
 
 	uint32_t exp_param_types = TEE_PARAM_TYPES(
-			TEE_PARAM_TYPE_VALUE_INPUT,
 			TEE_PARAM_TYPE_MEMREF_OUTPUT,
 			TEE_PARAM_TYPE_MEMREF_OUTPUT,
-			TEE_PARAM_TYPE_VALUE_OUTPUT);
+			TEE_PARAM_TYPE_VALUE_OUTPUT,
+			TEE_PARAM_TYPE_NONE);
 
 	if (param_types != exp_param_types)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	rsa_keypair_id = params[0].value.a;
-	buffer1 = params[1].memref.buffer;
-	buffer2 = params[2].memref.buffer;
+	rsa_keypair_id = RSA_KEY_ID;
+	buffer1 = params[0].memref.buffer;
+	buffer2 = params[1].memref.buffer;
 
-	buffer_len1 = params[1].memref.size;
-	buffer_len2 = params[2].memref.size;
+	buffer_len1 = params[0].memref.size;
+	buffer_len2 = params[1].memref.size;
 
 	result = TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE, &rsa_keypair_id, sizeof(rsa_keypair_id),
 			flags, &rsa_keypair);
@@ -209,7 +249,7 @@ static TEE_Result get_public_key_exponent_modulus(uint32_t param_types, TEE_Para
 	if (result != TEE_SUCCESS)
 	{
 		EMSG("Failed to open object handle : 0x%x", result);
-		params[3].value.a = 0;
+		params[2].value.a = 0;
 		goto cleanup;
 	}
 
@@ -218,7 +258,7 @@ static TEE_Result get_public_key_exponent_modulus(uint32_t param_types, TEE_Para
 	if (result != TEE_SUCCESS)
 	{
 		EMSG("Failed to get object buffer attribute. TEE_GetObjectBufferAttribute res: 0x%x", result);
-		params[3].value.a = 0;
+		params[2].value.a = 0;
 		goto cleanup1;
 	}
 
@@ -227,18 +267,152 @@ static TEE_Result get_public_key_exponent_modulus(uint32_t param_types, TEE_Para
 	if (result != TEE_SUCCESS)
 	{
 		EMSG("Failed to get object buffer attribute. TEE_GetObjectBufferAttribute res: 0x%x", result);
-		params[3].value.a = 0;
+		params[2].value.a = 0;
 		goto cleanup1;
 	}
 
-	params[1].memref.size = buffer_len1;
-	params[2].memref.size = buffer_len2;
-	params[3].value.a = 1;
+	params[0].memref.size = buffer_len1;
+	params[1].memref.size = buffer_len2;
+	params[2].value.a = 1;
 
 	cleanup1:
 	TEE_CloseObject(rsa_keypair);
 	cleanup:
 	return result;
+}
+
+static TEE_Result save_signed_public_key(uint32_t param_types, TEE_Param params[4])
+{
+	TEE_Result result = TEE_SUCCESS;
+	TEE_ObjectHandle transientKey = (TEE_ObjectHandle)NULL;
+	TEE_ObjectHandle persistentKey = (TEE_ObjectHandle)NULL;
+	TEE_ObjectInfo keyInfo;
+	TEE_ObjectInfo keyInfo2;
+	uint8_t *signed_pk;
+	uint32_t signedPublicKeyId = 0;
+	uint32_t signed_pk_len = 0;
+
+	uint32_t flags = TEE_DATA_FLAG_ACCESS_READ |
+			TEE_DATA_FLAG_ACCESS_WRITE |
+			TEE_DATA_FLAG_ACCESS_WRITE_META |
+			TEE_DATA_FLAG_SHARE_READ |
+			TEE_DATA_FLAG_SHARE_WRITE;
+
+	uint32_t exp_param_types = TEE_PARAM_TYPES(
+			TEE_PARAM_TYPE_MEMREF_INPUT,
+			TEE_PARAM_TYPE_VALUE_OUTPUT,
+			TEE_PARAM_TYPE_NONE,
+			TEE_PARAM_TYPE_NONE);
+
+	signedPublicKeyId = SIGNED_PUBLIC_KEY_ID;
+	signed_pk = params[0].memref.buffer;
+	signed_pk_len = params[0].memref.size;
+
+	if (param_types != exp_param_types)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	result = TEE_AllocateTransientObject(TEE_TYPE_GENERIC_SECRET, signed_pk_len, &transientKey);
+
+	if (result != TEE_SUCCESS) {
+		EMSG("Failed to allocate transient object handle : 0x%x", result);
+		params[1].value.a = 0;
+		goto cleanup;
+	}
+
+	result = TEE_WriteObjectData(transientKey, signed_pk, signed_pk_len);
+
+	if (result != TEE_SUCCESS) {
+		EMSG("Failed to write signed public key to a transient key: 0x%x", result);
+		params[1].value.a = 0;
+		goto cleanup1;
+	}
+
+	TEE_GetObjectInfo1(transientKey, &keyInfo);
+	result = TEE_CreatePersistentObject(TEE_STORAGE_PRIVATE, &signedPublicKeyId, sizeof(signedPublicKeyId),
+			flags, transientKey, NULL, 0, &persistentKey);
+
+	if (result != TEE_SUCCESS) {
+		EMSG("Failed to create a persistent key: 0x%x", result);
+		params[1].value.a = 0;
+		goto cleanup1;
+	}
+
+	TEE_GetObjectInfo1(persistentKey, &keyInfo2);
+	result = check_obj(&keyInfo, &keyInfo2);
+
+	if (result != TEE_SUCCESS) {
+		EMSG("keyInfo and keyInfo2 are different");
+		params[1].value.a = 0;
+		goto cleanup2;
+	}
+
+	params[1].value.a = 1;
+
+	cleanup2:
+	TEE_CloseObject(persistentKey);
+	cleanup1:
+	TEE_FreeTransientObject(transientKey);
+	cleanup:
+	return result;
+
+}
+
+static TEE_Result get_signed_public_key(uint32_t param_types, TEE_Param params[4])
+{
+	TEE_Result result = TEE_SUCCESS;
+	TEE_ObjectHandle signed_pk_handle = (TEE_ObjectHandle)NULL;
+
+	uint8_t *buffer;
+	uint32_t buffer_len = 0;
+
+	uint32_t signed_pk_id;
+
+	uint32_t flags = TEE_DATA_FLAG_ACCESS_READ |
+			TEE_DATA_FLAG_ACCESS_WRITE |
+			TEE_DATA_FLAG_ACCESS_WRITE_META |
+			TEE_DATA_FLAG_SHARE_READ |
+			TEE_DATA_FLAG_SHARE_WRITE;
+
+	uint32_t exp_param_types = TEE_PARAM_TYPES(
+			TEE_PARAM_TYPE_MEMREF_OUTPUT,
+			TEE_PARAM_TYPE_VALUE_OUTPUT,
+			TEE_PARAM_TYPE_NONE,
+			TEE_PARAM_TYPE_NONE);
+
+	if (param_types != exp_param_types)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	signed_pk_id = SIGNED_PUBLIC_KEY_ID;
+
+	buffer = params[0].memref.buffer;
+	buffer_len = params[0].memref.size;
+
+	result = TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE, &signed_pk_id, sizeof(signed_pk_id),
+			flags, &signed_pk_handle);
+
+	if (result != TEE_SUCCESS)
+	{
+		EMSG("Failed to open object handle : 0x%x", result);
+		params[2].value.a = 0;
+		goto cleanup;
+	}
+
+	result = TEE_GetObjectBufferAttribute(signed_pk_handle, TEE_ATTR_RSA_PUBLIC_EXPONENT, buffer, &buffer_len);
+	if (result != TEE_SUCCESS)
+	{
+		EMSG("Failed to get object buffer attribute. TEE_GetObjectBufferAttribute res: 0x%x", result);
+		params[2].value.a = 0;
+		goto cleanup1;
+	}
+
+	params[0].memref.size = buffer_len;
+	params[1].value.a = 1;
+
+	cleanup1:
+	TEE_CloseObject(signed_pk_handle);
+	cleanup:
+	return result;
+
 }
 
 static TEE_Result verify_message(uint32_t param_types, TEE_Param params[4])
@@ -263,8 +437,8 @@ static TEE_Result verify_message(uint32_t param_types, TEE_Param params[4])
 	uint32_t exp_param_types = TEE_PARAM_TYPES(
 			TEE_PARAM_TYPE_MEMREF_INPUT,
 			TEE_PARAM_TYPE_MEMREF_INPUT,
-			TEE_PARAM_TYPE_VALUE_INPUT,
-			TEE_PARAM_TYPE_VALUE_OUTPUT);
+			TEE_PARAM_TYPE_VALUE_OUTPUT,
+			TEE_PARAM_TYPE_NONE);
 
 	if (param_types != exp_param_types)
 		return TEE_ERROR_BAD_PARAMETERS;
@@ -275,14 +449,14 @@ static TEE_Result verify_message(uint32_t param_types, TEE_Param params[4])
 	signed_message = params[1].memref.buffer;
 	signed_message_len = params[1].memref.size;
 
-	keyId = params[2].value.a;
+	keyId = RSA_KEY_ID;
 
 	result = TEE_AllocateOperation(&operation, TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA256,
-			TEE_MODE_VERIFY, KEY_SIZE * 2);
+			TEE_MODE_VERIFY, RSA_KEY_SIZE * 2);
 
 	if (result != TEE_SUCCESS) {
 		EMSG("Failed to allocate operation: 0x%x", result);
-		params[3].value.a = 0;
+		params[2].value.a = 0;
 		goto cleanup;
 	}
 
@@ -291,7 +465,7 @@ static TEE_Result verify_message(uint32_t param_types, TEE_Param params[4])
 
 	if (result != TEE_SUCCESS) {
 		EMSG("Failed to open persistent key: 0x%x", result);
-		params[3].value.a = 0;
+		params[2].value.a = 0;
 		goto cleanup1;
 	}
 
@@ -299,7 +473,7 @@ static TEE_Result verify_message(uint32_t param_types, TEE_Param params[4])
 
 	if (result != TEE_SUCCESS) {
 		EMSG("Failed to set key: 0x%x", result);
-		params[3].value.a = 0;
+		params[2].value.a = 0;
 		goto cleanup2;
 	}
 
@@ -308,11 +482,11 @@ static TEE_Result verify_message(uint32_t param_types, TEE_Param params[4])
 
 	if (result == TEE_SUCCESS)
 	{
-		params[3].value.a = 1;
+		params[2].value.a = 1;
 	}
 	else
 	{
-		params[3].value.a = 0;
+		params[2].value.a = 0;
 	}
 
 	cleanup2:
@@ -330,14 +504,20 @@ TEE_Result TA_InvokeCommandEntryPoint(void *sess_ctx, uint32_t cmd_id,
 	(void)&sess_ctx;
 
 	switch (cmd_id) {
-		case TA_GENERATE_KEY_CMD:
-			return generate_and_save_key(param_types, params);
-		case TA_DELETE_KEY_CMD:
-			return delete_key(param_types, params);
+		case TA_GENERATE_RSA_KEY_CMD:
+			return generate_and_save_rsa_key(param_types, params);
+		case TA_DELETE_RSA_KEY_CMD:
+			return delete_rsa_key(param_types, params);
+		case TA_DELETE_SIGNED_PUBLIC_KEY_CMD:
+			return delete_signed_public_key(param_types, params);
 		case TA_GET_PUBLICKEY_EXP_MOD_CMD:
 			return get_public_key_exponent_modulus(param_types, params);
 		case TA_VERIFY_CMD:
 			return verify_message(param_types, params);
+		case TA_SAVE_SIGNED_PUBLIC_KEY_CMD:
+			return save_signed_public_key(param_types, params);
+		case TA_GET_SIGNED_PUBLIC_KEY_CMD:
+			return get_signed_public_key(param_types, params);
 		default:
 			return TEE_ERROR_BAD_PARAMETERS;
 	}
